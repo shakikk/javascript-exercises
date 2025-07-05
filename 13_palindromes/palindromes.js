17e456edecdf1a2fc4af33ebb6784634f4da7c7f
shakikk <shakik03@gmail.com>
2025-07-05 22:17:14 +0100

    completed assignments 13-16 of TOP javascript

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedWord = word
+     .toLowerCase()
+     .split('')
+     .filter((character) => alphanumerical.includes(character))
+     .join('');
+
+    const reversedWord = cleanedWord.split('').reverse().join('');
+
+    return reversedWord === cleanedWord;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = function(person){
+    if(!person.yearOfDeath){
+        yearOfDeath = new Date().getFullYear();
+        return yearOfDeath - person.yearOfBirth;
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+};
 
+const findTheOldest = function (people) {
+  const peopleOldestToYoungest = people.sort(
+    (person, nextPerson) => getAge(nextPerson) - getAge(person),
+  );
+
+  return peopleOldestToYoungest[0];
 };
 
 // Do not edit below this line
