191dfb6063da9366f6a1b524a353590eaa53c82a
shakikk <shakik03@gmail.com>
2025-07-05 18:20:54 +0100

    completed assignment 6-12 of TOP js fundamentals

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if(!Number.isInteger(min) || !Number.isInteger(max)){
+        return "ERROR"
+    }
+    if(min<0 || max<0){
+        return "ERROR";
+    }
+    let total = 0;
+    
+    if(min>max){
+        const temp = max;
+        max = min;
+        min = temp;
+    }
+    
+    for(let i = min; i<=max; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
